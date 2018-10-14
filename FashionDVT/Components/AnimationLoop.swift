2e9777729b1aa0adeace766b7cf8a2defe47cc45
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-10-14 15:11:34 -0300

    animation loop

---
+++ FashionDVT/Components/AnimationLoop.swift
@@ -0,0 +1,10 @@
+import Lottie
+
+class AnimationLoop: LOTAnimationView {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    contentMode = .scaleAspectFill
+    loopAnimation = true
+  }
+}
--- FashionDVT/GalleryMenuViewController.swift
+++ FashionDVT/GalleryMenuViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 
 class GalleryMenuViewController: UIViewController {
 
+  @IBOutlet weak var pic1: AnimationLoop!
+  @IBOutlet weak var pic2: AnimationLoop!
+  @IBOutlet weak var pic3: AnimationLoop!
+  @IBOutlet weak var pic4: AnimationLoop!
+  @IBOutlet weak var pic5: AnimationLoop!
+  @IBOutlet weak var pic6: AnimationLoop!
+  @IBOutlet weak var pic7: AnimationLoop!
+  @IBOutlet weak var pic8: AnimationLoop!
+  @IBOutlet weak var pic9: AnimationLoop!
+  @IBOutlet weak var pic10: AnimationLoop!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    pic1.setAnimation(named: "spinning_cat")
+    pic1.play()
   }
 
 }
