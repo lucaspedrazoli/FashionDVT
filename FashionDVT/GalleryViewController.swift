51fc5c1d57659ff24e7f4be70466d928036c2553
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-03 20:50:06 -0300

    collection view

--- FashionDVT/GalleryViewController.swift
+++ FashionDVT/GalleryViewController.swift
@@ -2,10 +2,25 @@ import UIKit
 import Hero
 import ViewAnimator
 
-class GalleryViewController: UIViewController {
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+  @IBOutlet weak var collectionView: UICollectionView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
 
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    cell.backgroundColor = .black
+    return cell
+  }
+
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
   }
 }
