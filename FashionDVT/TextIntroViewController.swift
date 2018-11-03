1b9a75d0f9a9f98c8e7019e8cfe35f688255aea3
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-02 21:33:20 -0300

    text and transition

--- FashionDVT/TextIntroViewController.swift
+++ FashionDVT/TextIntroViewController.swift
@@ -1,19 +1,49 @@
 import UIKit
 import RQShineLabel
+import TransitionButton
 
 class TextIntroViewController: UIViewController {
 
   @IBOutlet var label: RQShineLabel!
+  @IBOutlet var button: TransitionButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    label.numberOfLines = 0
-    label.text = "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime."
-    label.backgroundColor = .clear
-    label.fadeoutDuration = 5
-    label.shineDuration = 5
+    buttonLayout()
+    button.startAnimation()
+    labelLayout()
     label.shine(completion: {
-      self.label.fadeOut()
+      self.label.fadeOut(completion: {
+        self.button.stopAnimation()
+      })
     })
   }
+
+  @IBAction func navigate() {
+    button.stopAnimation(animationStyle: .expand, completion: {
+      let secondVC = UIViewController()
+      self.present(secondVC, animated: true, completion: nil)
+    })
+  }
+
+  private func buttonLayout() {
+    button.layer.borderColor = UIColor.white.cgColor
+    button.layer.borderWidth = 2
+    button.cornerRadius = 20
+    button.spinnerColor = .red
+    button.setTitle("iniciar", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    button.addTarget(self, action: #selector(navigate), for: .touchUpInside)
+  }
+
+  private func labelLayout() {
+  label.numberOfLines = 0
+  label.text = "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime."
+  label.backgroundColor = .clear
+  label.fadeoutDuration = 2
+  label.shineDuration = 4
+  }
 }
+
+
+
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ target 'FashionDVT' do
 
   pod 'lottie-ios'
   pod 'RQShineLabel'
+  pod 'TransitionButton'
 end
