a63e1437a778a5c1744196962120294a12897171
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-04 19:59:10 -0200

    pauta issue 1

--- FashionDVT/IssueOne/IssueOnePage1ViewController.swift
+++ FashionDVT/IssueOne/IssueOnePage1ViewController.swift
@@ -8,5 +8,26 @@ class IssueOnePage1ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    label.textColor = .lightGray
+    label.numberOfLines = 0
+    label.shineDuration = 5
+    label.fadeoutDuration = 3
+    label.text = "\"São tempos de superficialidade, ansiedade e incerteza, e as pessoas buscam preencher esse vazio por meio do consumo desenfreado das coisas\""
+    startAnimation()
   }
+
+  private func startAnimation() {
+    self.label.shine(completion: {
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3), execute: {
+        self.finishAnimation()
+      })
+    })
+  }
+
+  private func finishAnimation() {
+    self.label.fadeOut(completion: {
+      self.startAnimation()
+    })
+  }
+
 }
