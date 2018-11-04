4b92ceb98c3189766ac0408065bafea2bc4999c0
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-04 19:30:07 -0200

    page one label and text

--- FashionDVT/IssueOne/IssueOnePage1ViewController.swift
+++ FashionDVT/IssueOne/IssueOnePage1ViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
+import RQShineLabel
 
 class IssueOnePage1ViewController: UIViewController {
+
+  @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var label: RQShineLabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
