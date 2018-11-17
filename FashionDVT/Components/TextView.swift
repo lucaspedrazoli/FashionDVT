1fd3909a400f13b9806396a43bfd9977bcc82679
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-17 21:16:55 -0200

    text view

--- FashionDVT/Components/TextView.swift
+++ FashionDVT/Components/TextView.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class TextView: UITextView {
+class TextView: UITextView, UIGestureRecognizerDelegate {
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -8,7 +8,57 @@ class TextView: UITextView {
     backgroundColor = color
     layer.borderColor = UIColor.black.cgColor
     layer.borderWidth = 0.3
+    addDragGesture()
+  }
+
+  @objc func drag(_ recognizer: UIPanGestureRecognizer) {
+    let currentPoint = recognizer.location(in: self)
+    if recognizer.state == .changed {
+      frame.size.height = currentPoint.y
+      frame.size.width = currentPoint.x
+    } else if recognizer.state == .ended {
+      finishDrag()
+    }
+  }
+
+  @objc func textReadConfig() {
+    isScrollEnabled = true
+    isUserInteractionEnabled = true
+    flashScrollIndicators()
+    for recognizer in gestureRecognizers ?? [] {
+      removeGestureRecognizer(recognizer)
+    }
 
   }
 
+  private func addDragGesture() {
+    let recognizer = UIPanGestureRecognizer(target: self, action: #selector(drag(_:)))
+    recognizer.delegate = self
+    addGestureRecognizer(recognizer)
+  }
+
+  private func finishDrag() {
+    let dragLimit: CGFloat = 156 * 1.20
+    if frame.height > dragLimit {
+      changeSizeAnimation()
+    } else {
+      UIView.animate(withDuration: 0.3, animations: { [unowned self] in
+        self.resetHeight()
+      })
+    }
+  }
+
+  private func resetHeight() {
+    frame.size.height = 156
+    frame.size.width = 149
+  }
+
+  private func changeSizeAnimation() {
+    UIView.animate(withDuration: 0.3, animations: { [unowned self] in
+      self.frame.size.height = self.superview!.frame.height - 100
+      self.frame.size.width = self.superview!.frame.width - 30
+      }, completion: { [unowned self] (_) in
+        self.textReadConfig()
+    })
+  }
 }
--- FashionDVT/Components/TextViewController.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-class TextViewController: UIViewController {
-  
-}
