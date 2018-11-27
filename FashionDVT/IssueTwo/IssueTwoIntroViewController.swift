a928e7dbba89187c71fa3e9775a3a664bcb21606
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-11-26 23:18:08 -0200

    aline rocha test

--- FashionDVT/IssueTwo/IssueTwoIntroViewController.swift
+++ FashionDVT/IssueTwo/IssueTwoIntroViewController.swift
@@ -7,7 +7,7 @@ class IssueTwoViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    animationView.setAnimation(named: "anima_aline_rocha")
+    animationView.setAnimation(named: "anima_aline_rocha_v2")
     animationView.play(completion: { _ in
       self.performSegue(withIdentifier: "next", sender: nil)
     })
