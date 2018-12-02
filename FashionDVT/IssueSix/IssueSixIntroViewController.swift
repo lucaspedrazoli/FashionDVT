3566b48170ceaf677d4824a24d763cf68d644d9b
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2018-12-02 11:45:54 -0200

    bennuci animation bugfix

---
+++ FashionDVT/IssueSix/IssueSixIntroViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Lottie
+
+class IssueSixIntroViewController: UIViewController {
+  
+  @IBOutlet weak var animationView: AnimationView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    animationView.setAnimation(named: "anima_ensou_v2")
+    animationView.play(completion: { _ in
+      self.performSegue(withIdentifier: "next", sender: nil)
+    })
+  }
+}
